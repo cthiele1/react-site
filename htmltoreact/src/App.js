def7d55fc2f129c48b8ffdd54590979ac11a3d98
cthiele1 <cthiele@email.sc.edu>
2024-11-05 18:31:39 -0500

    just updated

--- htmltoreact/src/App.js
+++ htmltoreact/src/App.js
@@ -1,15 +1,25 @@
-import Header from "./components/Header";
-import Home from "./components/Home";
-import About from "./components/About";
 import "./styles/styles.css";
-import "./styles/about.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Admin from "./pages/Admin";
+import Recipes from "./pages/Recipes";
+import PopularCooks from "./pages/PopularCooks";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
-      <About />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="admin" element={<Admin />} />
+        <Route path="recipes" element={<Recipes />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="PopularCooks" element={<PopularCooks />} />
+      </Routes>
     </div>
   );
 }
---
+++ htmltoreact/src/pages/Recipes.js
@@ -0,0 +1,108 @@
+import React from "react";
+
+export default function Recipes() {
+  return (
+    <main id="main-content">
+      <div id="assignment-section">
+        <h2 id="Assignments" className="flex container">
+          <div id="container">
+            <section className="column">
+              <a href="#">
+                <h3>German Chocolate Cake</h3>
+                <img
+                  src="gcake.jpg"
+                  alt="Recipe 1 picture"
+                  height="200"
+                  width="200"
+                />
+              </a>
+              <p>
+                Even though the name may indicate otherwise, the German
+                Chocolate Cake actually has Southern roots dating back to the
+                1950s. The classic cake is defined by its decadent Coconut-Pecan
+                Frosting. Just make sure to let the cake layers cool completely
+                before lathering on the frosting.
+              </p>
+            </section>
+
+            <section className="column">
+              <a href="#">
+                <h3>Southern Fried Green Tomatoes</h3>
+              </a>
+              <img
+                src="friedtomato.jpg"
+                alt="Recipe 2 picture"
+                height="200"
+                width="200"
+              />
+              <p>
+                While certainly not the only way to enjoy green tomatoes, this
+                is simply the most delectable and Southern way to do it. The
+                best fried green tomatoes are crispy, lightly coated slices of
+                tangy green tomatoes, fried in vegetable oil or bacon grease.
+                Some buttermilk and a 50-50 blend of cornmeal and flour is all
+                you need.
+              </p>
+            </section>
+
+            <section className="column">
+              <a href="#">
+                <h3>Southern Skillet Cornbread</h3>
+              </a>
+              <img
+                src="skilletcornbread.jpg"
+                alt="Recipe 3 picture"
+                height="200"
+                width="200"
+              />
+              <p>
+                In our opinion, the only way to make classic cornbread is in a
+                cast-iron skillet. Plus, we skip the sugar and opt for bacon
+                drippings instead. Once you have a skillet, all you need is a
+                handful of ingredients and 10 minutes to get your cornbread
+                cooking.
+              </p>
+            </section>
+
+            <section className="column">
+              <a href="#">
+                <h3>Tomato, Cheddar, And Bacon Pie</h3>
+              </a>
+              <img
+                src="cheddarpie.jpg"
+                alt="Recipe 4 picture"
+                height="200"
+                width="200"
+              />
+              <p>
+                We love in-season tomatoes so much that we'll bake them into
+                pies. With its mayonnaise and shredded cheese, old-fashioned
+                tomato pie has a decidedly retro appeal. But unlike retro dishes
+                such as congealed salad, we'll chow this down any day.
+              </p>
+            </section>
+
+            <section className="column">
+              <a href="#">
+                <h3>Pimento Cheese</h3>
+              </a>
+              <img
+                src="pimento.jpg"
+                alt="Recipe 5 picture"
+                height="200"
+                width="200"
+              />
+              <p>
+                Pimiento cheese is one of those recipes that proves a recipe
+                doesn't have to be complicated to be a crowd-pleaser. It comes
+                together in just 15 minutes with no cooking required, just a
+                little bit of grating. Yes, grating your own cheese is vital for
+                this recipe, don't dare buy the pre-shredded kind.
+              </p>
+            </section>
+          </div>
+        </h2>
+      </div>
+    </main>
+  );
+}
